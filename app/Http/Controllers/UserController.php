18e687ad26e166f406c19b35cf7f6e415a54c810
raviyadav2017sln@gmail.com <raviyadav2017sln@gmail.com>
2022-05-18 18:21:59 +0530

    rsa

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use DB;
+use Hash;
+use Illuminate\Support\Arr;
+
+class UserController extends Controller
+{
+    //
+}
